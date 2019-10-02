56a1feb6b1b729d260806462b25f9951e2059621
Olay G <olayg@icloud.com>
2019-10-02 10:44:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/showmethelistmvvm/view/adapter/PersonAdapter.java
@@ -0,0 +1,4 @@
+package com.example.showmethelistmvvm.view.adapter;
+
+public class PersonAdapter {
+}
---
+++ app/src/main/java/com/example/showmethelistmvvm/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,4 @@
+package com.example.showmethelistmvvm.viewmodel;
+
+public class MainActivityViewModel {
+}
